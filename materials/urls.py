2060211ccb047a6679f62510dd7c6069447b426e
Michael <sorokajr678@bk.ru>
2025-06-26 16:38:41 +0300

    add generics for lessons

--- materials/urls.py
+++ materials/urls.py
@@ -1,5 +1,7 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet
+from .views import (CourseViewSet, LessonListApiView, LessonCreateApiView, LessonRetrieveApiView,
+                    LessonUpdateApiView, LessonDestroyApiView)
 
 
 app_name = 'materials'
@@ -8,5 +10,9 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-
+    path('lessons/', LessonListApiView.as_view(), name='lessons'),
+    path('lesson/create/', LessonCreateApiView.as_view(), name='lesson_create'),
+    path('lesson/<int:pk>/', LessonRetrieveApiView.as_view(), name='lesson'),
+    path('lesson/<int:pk>/update/', LessonUpdateApiView.as_view(), name='lesson_update'),
+    path('lesson/<int:pk>/delete/', LessonDestroyApiView.as_view(), name='lesson_delete'),
 ] + router.urls

b51791dd730c08b0b5a6636e93dca7d0038e0ac3
Michael <sorokajr678@bk.ru>
2025-06-30 19:13:01 +0300

    update serializers.py

--- materials/models.py
+++ materials/models.py
@@ -18,7 +18,7 @@ class Lesson(models.Model):
     name = models.CharField(max_length=150, verbose_name='Урок')
     preview = models.ImageField(upload_to='lesson_photo', blank=True, null=True, verbose_name="Превью", help_text='Загрузите превью урока')
     description = models.TextField(verbose_name='Описание')
-    video_url = models.URLField(verbose_name='Ссылка на видео')
+    video_url = models.URLField(verbose_name='Ссылка на видео', blank=True, null=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE,)
 
     def __str__(self):
--- materials/serializers.py
+++ materials/serializers.py
@@ -1,18 +1,19 @@
 from rest_framework import serializers
 from .models import Course, Lesson
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     count_lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializer(source='lesson_set', many=True, read_only=True)
 
     def get_count_lessons(self, obj):
         return obj.lesson_set.count()
 
     class Meta:
         model = Course
-        fields = ('name', 'preview', 'description', 'count_lessons')
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
+        fields = ('id','name', 'preview', 'description', 'count_lessons', 'lessons')

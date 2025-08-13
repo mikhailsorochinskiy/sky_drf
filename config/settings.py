dbee49a6baa8da5f1dad3cd499733f4c1c8ed665
Michael <sorokajr678@bk.ru>
2025-08-13 19:24:07 +0300

    new try9

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import sys
 from datetime import timedelta
 
 from dotenv import load_dotenv
@@ -92,6 +93,14 @@ DATABASES = {
         'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
+
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'test_db.sqlite3',  # Для тестов
+        }
+    }
 
 
 # Password validation

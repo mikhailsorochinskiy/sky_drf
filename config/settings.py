ac4525bbd19de7c0cad972e144e441c8ca46c318
Michael <sorokajr678@bk.ru>
2025-08-13 20:19:06 +0300

    new try9

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,8 @@ load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-
+print('DB Host:', os.getenv('POSTGRES_HOST'))
+print('DB Name:', os.getenv('POSTGRES_DB'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
@@ -94,13 +95,13 @@ DATABASES = {
     }
 }
 
-if 'test' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'test_db.sqlite3',  # Для тестов
-        }
-    }
+# if 'test' in sys.argv:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'test_db.sqlite3',  # Для тестов
+#         }
+#     }
 
 
 # Password validation
